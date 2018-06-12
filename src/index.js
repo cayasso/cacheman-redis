69ca9d3a3ea61ad3bb24d5e2c19ddb08b5fea9d8
cayasso <cayasso@gmail.com>
2018-06-12 11:17:22 -0600

    [Fix] changing tabs for spaces

--- src/index.js
+++ src/index.js
@@ -15,47 +15,47 @@ const parse = parser(redis).parse
 const noop = () => {}
 
 class RedisStore {
-	/**
+  /**
    * RedisStore constructor.
    *
    * @param {String|Object} options
    * @api public
    */
 
-	constructor(options = {}) {
-		if ('string' === typeof options) {
-			options = parse(options)
-		}
+  constructor(options = {}) {
+    if ('string' === typeof options) {
+      options = parse(options)
+    }
 
-		const { port, host, client, setex, password, database, prefix } = options
+    const { port, host, client, setex, password, database, prefix } = options
 
-		if ('function' === typeof setex) {
-			this.client = options
-		} else if (client) {
-			this.client = client
-		} else if (!port && !host) {
-			this.client = redis.createClient()
-		} else {
-			const opts = Object.assign({}, options, { prefix: null })
-			this.client = redis.createClient(port, host, opts)
-		}
+    if ('function' === typeof setex) {
+      this.client = options
+    } else if (client) {
+      this.client = client
+    } else if (!port && !host) {
+      this.client = redis.createClient()
+    } else {
+      const opts = Object.assign({}, options, { prefix: null })
+      this.client = redis.createClient(port, host, opts)
+    }
 
-		if (password) {
-			this.client.auth(password, (err) => {
-				if (err) throw err
-			})
-		}
+    if (password) {
+      this.client.auth(password, (err) => {
+        if (err) throw err
+      })
+    }
 
-		if (database) {
-			this.client.select(database, (err) => {
-				if (err) throw err
-			})
-		}
+    if (database) {
+      this.client.select(database, (err) => {
+        if (err) throw err
+      })
+    }
 
-		this.prefix = prefix || 'cacheman:'
-	}
+    this.prefix = prefix || 'cacheman:'
+  }
 
-	/**
+  /**
    * Get an entry.
    *
    * @param {String} key
@@ -63,21 +63,21 @@ class RedisStore {
    * @api public
    */
 
-	get(key, fn = noop) {
-		const k = `${this.prefix}${key}`
-		this.client.get(k, (err, data) => {
-			if (err) return fn(err)
-			if (!data) return fn(null, null)
-			data = data.toString()
-			try {
-				fn(null, JSON.parse(data))
-			} catch (e) {
-				fn(e)
-			}
-		})
-	}
+  get(key, fn = noop) {
+    const k = `${this.prefix}${key}`
+    this.client.get(k, (err, data) => {
+      if (err) return fn(err)
+      if (!data) return fn(null, null)
+      data = data.toString()
+      try {
+        fn(null, JSON.parse(data))
+      } catch (e) {
+        fn(e)
+      }
+    })
+  }
 
-	/**
+  /**
    * Set an entry.
    *
    * @param {String} key
@@ -87,33 +87,33 @@ class RedisStore {
    * @api public
    */
 
-	set(key, val, ttl, fn = noop) {
-		const k = `${this.prefix}${key}`
+  set(key, val, ttl, fn = noop) {
+    const k = `${this.prefix}${key}`
 
-		if ('function' === typeof ttl) {
-			fn = ttl
-			ttl = null
-		}
+    if ('function' === typeof ttl) {
+      fn = ttl
+      ttl = null
+    }
 
-		try {
-			val = JSON.stringify(val)
-		} catch (e) {
-			return fn(e)
-		}
+    try {
+      val = JSON.stringify(val)
+    } catch (e) {
+      return fn(e)
+    }
 
-		const cb = (err) => {
-			if (err) return fn(err)
-			fn(null, val)
-		}
+    const cb = (err) => {
+      if (err) return fn(err)
+      fn(null, val)
+    }
 
-		if (-1 === ttl) {
-			this.client.set(k, val, cb)
-		} else {
-			this.client.setex(k, ttl || 60, val, cb)
-		}
-	}
+    if (-1 === ttl) {
+      this.client.set(k, val, cb)
+    } else {
+      this.client.setex(k, ttl || 60, val, cb)
+    }
+  }
 
-	/**
+  /**
    * Delete entry. Supported glob-style patterns.
    *
    * @param {String} key
@@ -121,14 +121,14 @@ class RedisStore {
    * @api private
    */
 
-	_del(key, fn = noop) {
-		this.client.del(key, (err) => {
-			if (err) return fn(err)
-			fn(null, null)
-		})
-	}
+  _del(key, fn = noop) {
+    this.client.del(key, (err) => {
+      if (err) return fn(err)
+      fn(null, null)
+    })
+  }
 
-	/**
+  /**
    * Delete an entry (Supported glob-style patterns).
    *
    * @param {String} key
@@ -136,35 +136,35 @@ class RedisStore {
    * @api public
    */
 
-	del(key, fn = noop) {
-		this._del(`${this.prefix}${key}`, fn)
-	}
+  del(key, fn = noop) {
+    this._del(`${this.prefix}${key}`, fn)
+  }
 
-	/**
+  /**
    * Clear all entries in cache.
    *
    * @param {Function} fn
    * @api public
    */
 
-	clear(fn = noop) {
-		this.client.keys(`${this.prefix}*`, (err, data) => {
-			if (err) return fn(err)
-			let count = data.length
-			if (count === 0) return fn(null, null)
-			data.forEach((key) => {
-				this.client.del(key, (err) => {
-					if (err) {
-						count = 0
-						return fn(err)
-					}
-					if (--count == 0) {
-						fn(null, null)
-					}
-				})
-			})
-		})
-	}
+  clear(fn = noop) {
+    this.client.keys(`${this.prefix}*`, (err, data) => {
+      if (err) return fn(err)
+      let count = data.length
+      if (count === 0) return fn(null, null)
+      data.forEach((key) => {
+        this.client.del(key, (err) => {
+          if (err) {
+            count = 0
+            return fn(err)
+          }
+          if (--count == 0) {
+            fn(null, null)
+          }
+        })
+      })
+    })
+  }
 }
 
 module.exports = RedisStore
--- test/index.js
+++ test/index.js
@@ -4,214 +4,214 @@ const Cache = require('../src')
 
 const uri = 'redis://127.0.0.1:6379/5'
 const connection = {
-	host: '127.0.0.1',
-	port: 6379,
-	database: 6
+  host: '127.0.0.1',
+  port: 6379,
+  database: 6
 }
 
 describe('cacheman-redis', () => {
-	let cache = null
+  let cache = null
 
-	beforeEach((done) => {
-		cache = new Cache()
-		done()
-	})
+  beforeEach((done) => {
+    cache = new Cache()
+    done()
+  })
 
-	afterEach((done) => {
-		cache.clear(done)
-	})
+  afterEach((done) => {
+    cache.clear(done)
+  })
 
-	it('should have main methods', () => {
-		assert.ok(cache.set)
-		assert.ok(cache.get)
-		assert.ok(cache.del)
-		assert.ok(cache.clear)
-	})
+  it('should have main methods', () => {
+    assert.ok(cache.set)
+    assert.ok(cache.get)
+    assert.ok(cache.del)
+    assert.ok(cache.clear)
+  })
 
-	it('should store items', (done) => {
-		cache.set('test1', { a: 1 }, (err) => {
-			if (err) return done(err)
-			cache.get('test1', (err, data) => {
-				if (err) return done(err)
-				assert.equal(data.a, 1)
-				done()
-			})
-		})
-	})
+  it('should store items', (done) => {
+    cache.set('test1', { a: 1 }, (err) => {
+      if (err) return done(err)
+      cache.get('test1', (err, data) => {
+        if (err) return done(err)
+        assert.equal(data.a, 1)
+        done()
+      })
+    })
+  })
 
-	it('should store zero', (done) => {
-		cache.set('test2', 0, (err) => {
-			if (err) return done(err)
-			cache.get('test2', (err, data) => {
-				if (err) return done(err)
-				assert.strictEqual(data, 0)
-				done()
-			})
-		})
-	})
+  it('should store zero', (done) => {
+    cache.set('test2', 0, (err) => {
+      if (err) return done(err)
+      cache.get('test2', (err, data) => {
+        if (err) return done(err)
+        assert.strictEqual(data, 0)
+        done()
+      })
+    })
+  })
 
-	it('should store false', (done) => {
-		cache.set('test3', false, (err) => {
-			if (err) return done(err)
-			cache.get('test3', (err, data) => {
-				if (err) return done(err)
-				assert.strictEqual(data, false)
-				done()
-			})
-		})
-	})
+  it('should store false', (done) => {
+    cache.set('test3', false, (err) => {
+      if (err) return done(err)
+      cache.get('test3', (err, data) => {
+        if (err) return done(err)
+        assert.strictEqual(data, false)
+        done()
+      })
+    })
+  })
 
-	it('should store null', (done) => {
-		cache.set('test4', null, (err) => {
-			if (err) return done(err)
-			cache.get('test4', (err, data) => {
-				if (err) return done(err)
-				assert.strictEqual(data, null)
-				done()
-			})
-		})
-	})
+  it('should store null', (done) => {
+    cache.set('test4', null, (err) => {
+      if (err) return done(err)
+      cache.get('test4', (err, data) => {
+        if (err) return done(err)
+        assert.strictEqual(data, null)
+        done()
+      })
+    })
+  })
 
-	it('should delete items', (done) => {
-		let value = Date.now()
-		cache.set('test5', value, (err) => {
-			if (err) return done(err)
-			cache.get('test5', (err, data) => {
-				if (err) return done(err)
-				assert.equal(data, value)
-				cache.del('test5', (err) => {
-					if (err) return done(err)
-					cache.get('test5', (err, data) => {
-						if (err) return done(err)
-						assert.equal(data, null)
-						done()
-					})
-				})
-			})
-		})
-	})
+  it('should delete items', (done) => {
+    let value = Date.now()
+    cache.set('test5', value, (err) => {
+      if (err) return done(err)
+      cache.get('test5', (err, data) => {
+        if (err) return done(err)
+        assert.equal(data, value)
+        cache.del('test5', (err) => {
+          if (err) return done(err)
+          cache.get('test5', (err, data) => {
+            if (err) return done(err)
+            assert.equal(data, null)
+            done()
+          })
+        })
+      })
+    })
+  })
 
-	it('should clear items', (done) => {
-		let value = Date.now()
-		cache.set('test6', value, (err) => {
-			if (err) return done(err)
-			cache.get('test6', (err, data) => {
-				if (err) return done(err)
-				assert.equal(data, value)
-				cache.clear((err) => {
-					if (err) return done(err)
-					cache.get('test6', (err, data) => {
-						if (err) return done(err)
-						assert.equal(data, null)
-						done()
-					})
-				})
-			})
-		})
-	})
+  it('should clear items', (done) => {
+    let value = Date.now()
+    cache.set('test6', value, (err) => {
+      if (err) return done(err)
+      cache.get('test6', (err, data) => {
+        if (err) return done(err)
+        assert.equal(data, value)
+        cache.clear((err) => {
+          if (err) return done(err)
+          cache.get('test6', (err, data) => {
+            if (err) return done(err)
+            assert.equal(data, null)
+            done()
+          })
+        })
+      })
+    })
+  })
 
-	it('should expire key', function(done) {
-		this.timeout(0)
-		cache.set('test7', { a: 1 }, 1, (err) => {
-			if (err) return done(err)
-			setTimeout(() => {
-				cache.get('test7', (err, data) => {
-					if (err) return done(err)
-					assert.equal(data, null)
-					done()
-				})
-			}, 1100)
-		})
-	})
+  it('should expire key', function(done) {
+    this.timeout(0)
+    cache.set('test7', { a: 1 }, 1, (err) => {
+      if (err) return done(err)
+      setTimeout(() => {
+        cache.get('test7', (err, data) => {
+          if (err) return done(err)
+          assert.equal(data, null)
+          done()
+        })
+      }, 1100)
+    })
+  })
 
-	it('should not expire key', function(done) {
-		this.timeout(0)
-		cache.set('test8', { a: 1 }, -1, (err) => {
-			if (err) return done(err)
-			setTimeout(() => {
-				cache.get('test8', (err, data) => {
-					if (err) return done(err)
-					assert.deepEqual(data, { a: 1 })
-					done()
-				})
-			}, 1000)
-		})
-	})
+  it('should not expire key', function(done) {
+    this.timeout(0)
+    cache.set('test8', { a: 1 }, -1, (err) => {
+      if (err) return done(err)
+      setTimeout(() => {
+        cache.get('test8', (err, data) => {
+          if (err) return done(err)
+          assert.deepEqual(data, { a: 1 })
+          done()
+        })
+      }, 1000)
+    })
+  })
 
-	it('should get the same value subsequently', (done) => {
-		let val = 'Test Value'
-		cache.set('test', 'Test Value', () => {
-			cache.get('test', (err, data) => {
-				if (err) return done(err)
-				assert.strictEqual(data, val)
-				cache.get('test', (err, data) => {
-					if (err) return done(err)
-					assert.strictEqual(data, val)
-					cache.get('test', (err, data) => {
-						if (err) return done(err)
-						assert.strictEqual(data, val)
-						done()
-					})
-				})
-			})
-		})
-	})
+  it('should get the same value subsequently', (done) => {
+    let val = 'Test Value'
+    cache.set('test', 'Test Value', () => {
+      cache.get('test', (err, data) => {
+        if (err) return done(err)
+        assert.strictEqual(data, val)
+        cache.get('test', (err, data) => {
+          if (err) return done(err)
+          assert.strictEqual(data, val)
+          cache.get('test', (err, data) => {
+            if (err) return done(err)
+            assert.strictEqual(data, val)
+            done()
+          })
+        })
+      })
+    })
+  })
 
-	it('should allow passing redis connection params as object', (done) => {
-		cache = new Cache(connection)
-		cache.set('test9', { a: 1 }, (err) => {
-			if (err) return done(err)
-			cache.get('test9', (err, data) => {
-				if (err) return done(err)
-				assert.equal(data.a, 1)
-				assert.equal(cache.client.selected_db, 6)
-				done()
-			})
-		})
-	})
+  it('should allow passing redis connection params as object', (done) => {
+    cache = new Cache(connection)
+    cache.set('test9', { a: 1 }, (err) => {
+      if (err) return done(err)
+      cache.get('test9', (err, data) => {
+        if (err) return done(err)
+        assert.equal(data.a, 1)
+        assert.equal(cache.client.selected_db, 6)
+        done()
+      })
+    })
+  })
 
-	it('should allow passing redis connection string', (done) => {
-		cache = new Cache(uri)
-		cache.set('test10', { a: 1 }, (err) => {
-			if (err) return done(err)
-			cache.get('test10', (err, data) => {
-				if (err) return done(err)
-				assert.equal(data.a, 1)
-				assert.equal(cache.client.selected_db, 5)
-				done()
-			})
-		})
-	})
+  it('should allow passing redis connection string', (done) => {
+    cache = new Cache(uri)
+    cache.set('test10', { a: 1 }, (err) => {
+      if (err) return done(err)
+      cache.get('test10', (err, data) => {
+        if (err) return done(err)
+        assert.equal(data.a, 1)
+        assert.equal(cache.client.selected_db, 5)
+        done()
+      })
+    })
+  })
 
-	it('should allow passing redis client as first argument', (done) => {
-		let client = redis.createClient()
-		cache = new Cache(client)
-		cache.set('test11', { a: 1 }, (err) => {
-			if (err) return done(err)
-			cache.get('test11', (err, data) => {
-				if (err) return done(err)
-				assert.equal(data.a, 1)
-				done()
-			})
-		})
-	})
+  it('should allow passing redis client as first argument', (done) => {
+    let client = redis.createClient()
+    cache = new Cache(client)
+    cache.set('test11', { a: 1 }, (err) => {
+      if (err) return done(err)
+      cache.get('test11', (err, data) => {
+        if (err) return done(err)
+        assert.equal(data.a, 1)
+        done()
+      })
+    })
+  })
 
-	it('should allow passing redis client as client in options', (done) => {
-		let client = redis.createClient()
-		cache = new Cache({ client: client })
-		cache.set('test12', { a: 1 }, (err) => {
-			if (err) return done(err)
-			cache.get('test12', (err, data) => {
-				if (err) return done(err)
-				assert.equal(data.a, 1)
-				done()
-			})
-		})
-	})
+  it('should allow passing redis client as client in options', (done) => {
+    let client = redis.createClient()
+    cache = new Cache({ client: client })
+    cache.set('test12', { a: 1 }, (err) => {
+      if (err) return done(err)
+      cache.get('test12', (err, data) => {
+        if (err) return done(err)
+        assert.equal(data.a, 1)
+        done()
+      })
+    })
+  })
 
-	it('should clear an empty cache', (done) => {
-		cache.clear((err) => {
-			done(err)
-		})
-	})
+  it('should clear an empty cache', (done) => {
+    cache.clear((err) => {
+      done(err)
+    })
+  })
 })
